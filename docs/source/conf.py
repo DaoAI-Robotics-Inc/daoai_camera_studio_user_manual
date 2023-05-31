44f0d72022c3b020d42da0673af6a8c048f99eee
Tom Zhang <tzhang@daoai.com>
2023-05-31 12:31:34 -0700

    change pdf engine

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 # -- Options to Support pdf build in chinese
-latex_engine = 'xelatex'
+latex_engine = 'lualatex'
 latex_use_xindy = False
 latex_elements = {
     'preamble': '\\usepackage[UTF8]{ctex}\n',
